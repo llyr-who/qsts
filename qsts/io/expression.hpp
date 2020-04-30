febd788fcc3cefef985c2ac27544b49c05eb650c
llyr-who <jamesbushnell1992@gmail.com>
2020-04-30 16:20:25 +0100

    node factory and epxression class complete

--- qsts/io/expression.hpp
+++ qsts/io/expression.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
-#include "algo/convert.hpp"
 #include "types/string.hpp"
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <type_traits>
 
 namespace qsts {
 
@@ -9,32 +14,66 @@ namespace qsts {
 // be moved to an external repo.
 namespace external_types {
 
-template <typename node_type>
+enum class node_type { variable, constant, binary_operation };
+
 class node {
 public:
-    node(node_type t);
-
-private:
-    node_type t_;
+    virtual node_type type() = 0;
 };
 
-namespace node_type {
-    struct variable {
-        std::string s_;
-    };
+class variable : public node {
+public:
+    variable(const char& v) : v_(v){};
+    node_type type() { return node_type::variable; }
 
-    struct constant {
-        double d_;
-    };
+private:
+    char v_;
+};
 
-    struct binary_operation {
-        node<binary_operation>* left;
-        node<binary_operation>* right;
-    };
+class constant : public node {
+public:
+    constant(const char& c) : c_(c){};
+    node_type type() { return node_type::variable; }
+
+private:
+    int c_;
+};
+
+class binary_operation : public node {
+public:
+    binary_operation(const char& op, std::shared_ptr<node> l,
+                     std::shared_ptr<node> r)
+        : op_(op), left_(l), right_(r){};
+    node_type type() { return node_type::variable; }
+
+private:
+    char op_;
+    std::shared_ptr<node> left_;
+    std::shared_ptr<node> right_;
+};
+
+//! factory for node generation
+//! we will be iterating over strings
+//! so the first argument is a const char&
+std::shared_ptr<node> make_node(const char& t,
+                                std::shared_ptr<node> lhs = nullptr,
+                                std::shared_ptr<node> rhs = nullptr) {
+    // if it is a char then it could be a variable
+    if (std::isalpha(t)) {
+        return std::make_shared<variable>(t);
+    }
+    if (std::isdigit(t)) {
+        return std::make_shared<constant>(t);
+    }
+    if (lhs == nullptr || rhs == nullptr) {
+        throw std::runtime_error("one of the operands is null");
+    }
+    return std::make_shared<binary_operation>(t, lhs, rhs);
 }
 
-
-class expression {};
+class expression {
+    std::unique_ptr<node> root_;
+};
 
 }  // namespace external_types
 
@@ -42,6 +81,7 @@ class expression {};
 postfix convert(const infix&);
 external_types::expression convert(const postfix&);
 
+// factory for expression generation.
 external_types::expression expression(const infix& ifx) {
     // convert infix to postfix
     auto pfx = convert(ifx);
